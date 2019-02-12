758d8d7c6079672ea8d5fd8039327e51f34479c7
Yurgen <ignatyug@gmail.com>
2019-02-13 00:46:48 +0200

    Task2

--- DataBase/database.py
+++ DataBase/database.py
@@ -1,7 +1,12 @@
 def dream():
     database = {'Petr Petrov': 'petr@gmail,com', 'John Smith': '+380-67234-55-55', 'Sasha Ivanov': 'ivanov@gmail.com'}
+    print('Before you start searching for a contact, you should know', '\n',
+          'that after the \"Stop\" ''command, you will complete the following contacts.', '\n')
     while True:
         name = input('Enter your username from your directory reference:   ')
+        if name == 'Stop':
+            print('I found what I was looking for.')
+            break
         if name in database:
             print(database.get(name))
         else:
