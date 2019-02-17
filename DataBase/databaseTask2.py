3d2ca482ba48783d302e6051de10833945c2dbb8
Yurgen <ignatyug@gmail.com>
2019-02-18 00:36:29 +0200

    Task2

---
+++ DataBase/databaseTask2.py
@@ -0,0 +1,18 @@
+
+def dream():
+    database = {'Petr Petrov': 'petr@gmail,com', 'John Smith': '+380-67234-55-55', 'Sasha Ivanov': 'ivanov@gmail.com'}
+    print('User remember, if you want to stop the search, enter the \"stop\" ''command', '\n')
+    name = 'txt'
+    with open(name, 'w') as f:
+        f.write(str(database))
+    name = 0
+    while name != 'stop':
+        name = input('Enter your username from your directory reference:   ')
+        if name in database:
+            print(database.get(name))
+        else:
+            print('You entered an invalid parameter, try again')
+        name = input('If you want to continue, press enter:   ')
+
+
+dream()
